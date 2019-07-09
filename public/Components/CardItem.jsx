64d755368f2b191f9ecd00c4d78de7ad288a03e8
dauzmark <52662371+dauzmark@users.noreply.github.com>
2019-07-09 16:51:57 +0100

    Edit
    change from class component to functional component

--- public/Components/CardItem.jsx
+++ public/Components/CardItem.jsx
@@ -1,35 +1,32 @@
 import React from "react";
 
-export default class CardItem extends React.Component {
-  render() {
-    // console.log(this.props.users);
-    const users = this.props.users.map(user => {
-      return (
-        <div
-          onClick={this.props.handleClick.bind(this, user)}
-          className="card m-3"
-          style={{ width: "10rem", height: "auto" }}
-          key={1 + Math.random()}
-        >
-          <img src={user.picture.large} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">
-              {user.name.first.toUpperCase()} {user.name.last.toUpperCase()}
-            </h5>
-            <p
-              className="card-text"
-              style={{ fontSize: "12px", color: "#00ccff" }}
-            >
-              {user.email}
-            </p>
-            <p style={{ fontSize: "12px" }}>
-              {user.location.street}, {user.location.city},{" "}
-              {user.location.state}
-            </p>
-          </div>
+export default function CardItem(props) {
+  // console.log(this.props.users);
+  const users = props.users.map(user => {
+    return (
+      <div
+        onClick={props.handleClick.bind(this, user)}
+        className="card m-3"
+        style={{ width: "10rem", height: "auto" }}
+        key={1 + Math.random()}
+      >
+        <img src={user.picture.large} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">
+            {user.name.first.toUpperCase()} {user.name.last.toUpperCase()}
+          </h5>
+          <p
+            className="card-text"
+            style={{ fontSize: "12px", color: "#00ccff" }}
+          >
+            {user.email}
+          </p>
+          <p style={{ fontSize: "12px" }}>
+            {user.location.street}, {user.location.city}, {user.location.state}
+          </p>
         </div>
-      );
-    });
-    return <>{users}</>;
-  }
+      </div>
+    );
+  });
+  return <>{users}</>;
 }
